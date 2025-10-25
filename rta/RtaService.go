f922e6c08ac3c6e501c4d0d4522d1021cd0f4d21
ayouman95 <156737219+ayouman95@users.noreply.github.com>
2025-10-25 15:51:04 +0800

    update rta timeout

--- rta/RtaService.go
+++ rta/RtaService.go
@@ -51,7 +51,7 @@ var (
 		CheckRedirect: func(req *http.Request, via []*http.Request) error {
 			return http.ErrUseLastResponse
 		},
-		Timeout: 1 * time.Second, // 整个请求超时
+		Timeout: 2 * time.Second, // 整个请求超时
 	}
 )
 
